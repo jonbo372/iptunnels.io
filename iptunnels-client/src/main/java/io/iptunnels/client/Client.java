4887d3f2ee63af1e3710e3da8f6bc8e3720e96c5
Jonas Borjesson <jonas@jonasborjesson.com>
2018-09-23 00:13:35 -0700

    working quit well

--- iptunnels-client/src/main/java/io/iptunnels/client/Client.java
+++ iptunnels-client/src/main/java/io/iptunnels/client/Client.java
@@ -18,7 +18,9 @@ public class Client {
     public static void main(final String...args) throws Exception {
 
         // the iptunnels.io server.
-        final String host = "127.0.0.1";
+        // final String host = "127.0.0.1";
+        // final String host = "104.248.212.248";
+        final String host = "159.89.220.227";
         final int port = 8000;
 
         // the local target to which we will relay all packets.
